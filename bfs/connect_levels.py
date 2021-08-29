a59a6d096a52c68aa21985bbe881b3d376fab7f1
seamusmalley <seamalley@gmail.com>
2021-08-29 13:21:31 -0400

    8/29/2021

--- bfs/connect_levels.py
+++ bfs/connect_levels.py
@@ -1,19 +1,13 @@
-"""
-Given a binary tree and a node, find the level order successor of the given node in the tree.
-The level order successor is the node that appears right after the given node in the level order traversal.
-"""
-
 class Node:
     def __init__(self, value: int):
         self.value = value
         self.left = None
         self.right = None
+        self.next = None
 
-def find_lo_successor(root: Node, id: int) -> int:
-    traversal = []
-
+def connect_levels(root: Node) -> None:
     if not root:
-        return traversal
+        return
 
     queue = [root]
 
@@ -22,21 +16,30 @@ def find_lo_successor(root: Node, id: int) -> int:
 
         for _ in range(len(queue)):
             node = queue.pop(0)
-            level.append(node.value)
+            level.append(node)
 
             if node.left:
                 queue.append(node.left)
             if node.right:
                 queue.append(node.right)
 
-        traversal.extend(level)
+        for i in range(len(level)-1):
+            level[i].next = level[i+1]
 
-    for i in range(len(traversal)):
-        if traversal[i] == id:
-            return traversal[i + 1]
 
-    return -1
-
+def print_levels(root: Node) -> None:
+    print('Tree Levels')
+    i = 0
+    node = root
+    while node:
+        root = node
+        level = [node.value]
+        while node.next:
+            level.append(node.next.value)
+            node = node.next
+        print(i, ':', level)
+        node = root.left
+        i += 1
 
 # Test 1
 root = Node(1)
@@ -44,7 +47,10 @@ root.left = Node(2)
 root.left.left = Node(4)
 root.left.right = Node(5)
 root.right = Node(3)
-print(find_lo_successor(root, 3))
+root.right.left = Node(6)
+root.right.right = Node(7)
+connect_levels(root)
+print_levels(root)
 
 # Test 2
 root = Node(12)
@@ -53,8 +59,5 @@ root.left.left = Node(9)
 root.right = Node(1)
 root.right.left = Node(10)
 root.right.right = Node(5)
-print(find_lo_successor(root, 9))
-
-# Test 3
-print(find_lo_successor(root, 12))
-
+connect_levels(root)
+print_levels(root)
